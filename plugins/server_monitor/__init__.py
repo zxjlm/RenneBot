80a5f49e3fd3096d0506de599842ee5ab91b7218
zhang.xinjian <zhang.xinjian@astute-tec.com>
2021-11-26 22:11:07 +0800

    init server monitor

---
+++ plugins/server_monitor/__init__.py
@@ -0,0 +1,12 @@
+class ServerMonitor:
+    def __init__(self):
+        pass
+
+    def config_server_monitor(self):
+        pass
+
+    def get_server_monitor_config(self):
+        pass
+
+    def get_server_status(self):
+        pass
